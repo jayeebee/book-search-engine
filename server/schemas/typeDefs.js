4ed86f5f4c907609dc80218f1a733fd306954245
jayeebee <pnuthexsa311@gmail.com>
2021-07-11 23:25:16 -0400

    setting up schemas based on assignement

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type Book {
+      bookId()
+      authors()
+      description
+      title
+      image
+      link
+  }
+`;
+
+module.exports = typeDefs;
