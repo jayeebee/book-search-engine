c76cbd8c63e7fbd0bcc3d5b76e5dd80a0e6d538e
jayeebee <pnuthexsa311@gmail.com>
2021-07-18 22:04:38 -0400

    fixed error in typedefs that was causing graphql error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,47 +1,46 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: SavedBook!): User
+    removeBook(bookId: ID!): User
+  }
+
   type User {
-    _id: ID
-    username: String
+    _id: ID!
+    username: String!
     email: String
     bookCount: Int
     savedBooks: [Book]
   }
 
-  type Auth {
-    token: ID!
-    user: User
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  input SavedBook {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
   }
 
   type Query {
     me: User
-  }
-
-  input SavedBook {
-      authors: [String]
-      description: String
-      title: String
-      bookId: String
-      image: String
-      link: String
-  }
-
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook: (input: SavedBook!): User
-    removeBook: (bookId: ID!): User
-  }
-
-  type Book {
-      _id: ID!
-      bookId: String
-      authors: [String]
-      description: String
-      title: String
-      image: String
-      link: String
+  }  
+  
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
